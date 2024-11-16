5d118f03ba5c437dda6a8e33425ddbeee6850359
Nijat K <neej@nijats-mbp.mynetworksettings.com>
2024-11-16 00:38:24 -0500

    Update lint
    Signed-off-by: Nijat K <neej@nijats-mbp.mynetworksettings.com>

--- csp/tests/adapters/test_websocket.py
+++ csp/tests/adapters/test_websocket.py
@@ -423,7 +423,7 @@ class TestWebsocket:
             # adapters, the websocket connection is not actually made.
             ws.send(csp.null_ts(str))
             assert ws._properties["port"] == "443"
-            stop_flag = csp.filter(csp.count(ws.status())==2,  ws.status())
+            stop_flag = csp.filter(csp.count(ws.status()) == 2, ws.status())
             csp.stop_engine(stop_flag)
 
         csp.run(g, starttime=datetime.now(pytz.UTC), realtime=True)
